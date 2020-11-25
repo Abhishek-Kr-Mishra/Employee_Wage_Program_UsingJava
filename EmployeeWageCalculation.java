b10b23a09e5cf1f21cf029a0a7901d466b29d5ff
abh224272 <abh224272@gmail.com>
2020-11-25 13:43:01 +0530

    Added: Condition to calculate the salary

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -9,13 +9,14 @@ public class EmployeeWageCalculation {
 		System.out.println("!!!!!!!!!! Welcome to Employee Wage Program !!!!!!!!!!");
 		
 		int WAGE_PER_HOUR=20;
-		int DAYS_IN_A_MONTH=20;
+		int totalWorkingHours=0;
+		int totalWorkingDays=0;
 		int monthSalary=0;
 		int workHour=0;
 		int perDaySalary=0;
 		
 		
-		for(int i=1; i<=DAYS_IN_A_MONTH; i++) {
+		while(totalWorkingHours<100 && totalWorkingDays<20) {
 	
 			Random random = new Random();
 			int randomValue=random.nextInt(3)+1;
@@ -23,25 +24,32 @@ public class EmployeeWageCalculation {
 			switch(randomValue) {
 			case 1:
 				workHour=8;
+				totalWorkingHours=totalWorkingHours+workHour;
 				break;
 			
 			case 2:
 				workHour=4;
+				totalWorkingHours=totalWorkingHours+workHour;
 				break;
 		
 			case 3:
 				workHour=0;
+				totalWorkingHours=totalWorkingHours+workHour;
 				break;
 			
 			default:
 				System.out.println("Invalid Case");
 		}
 
+			totalWorkingDays++;
 			perDaySalary=WAGE_PER_HOUR*workHour;
 			monthSalary=monthSalary+perDaySalary;
-			System.out.println("Salary of Day "+ i +" = "+perDaySalary);
-	}
+			System.out.println("Salary of Day "+ totalWorkingDays +" = "+perDaySalary);
+		}
 		
-		System.out.println("Total Salary of the month ="+monthSalary);
+			System.out.println("Total Working Hour= "+totalWorkingHours);
+			System.out.println("Total Slary of the Month= "+monthSalary);
+		
 	}
+	
 }
