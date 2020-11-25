ede2f5fd1512ea797c0957f03aa55526249d882e
abh224272 <abh224272@gmail.com>
2020-11-25 13:04:17 +0530

    Added: Part Time Employee and Per Day Salary

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -9,21 +9,31 @@ public class EmployeeWageCalculation {
 		System.out.println("!!!!!!!!!! Welcome to Employee Wage Program !!!!!!!!!!");
 		
 		int WAGE_PER_HOUR=20;
-		int workHour=8;
+		int workHour=0;
 		int perDaySalary=0;
-		
 		int isPresent=1;
+		int isFullTime=1;
+		
 		Random random = new Random();
 		int randomValue=random.nextInt(2);
 		
 		if(randomValue==isPresent) {
-			perDaySalary=WAGE_PER_HOUR*workHour;
-			System.out.println("Employee Is Present");
-			System.out.println("Salary= "+perDaySalary);
+			int fullOrPart=random.nextInt(2);
+			if(fullOrPart==isFullTime) {
+				System.out.println("Employee is Present and Full Time ");
+				workHour=8;
+			}
+			else {
+				System.out.println("Employee is Present and Part Time");
+				workHour=4;
+			}
 		}
 		else {
 			System.out.println("Employee is Absent");
-			System.out.println("Salary= "+perDaySalary);
 		}
+		
+		perDaySalary=WAGE_PER_HOUR*workHour;
+		System.out.println("Salary= "+perDaySalary);
+		
 	}
 }
