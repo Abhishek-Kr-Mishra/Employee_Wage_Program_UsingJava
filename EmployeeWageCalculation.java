3a809fd73f27be913488dcd7ed532f04044ac96b
abh224272 <abh224272@gmail.com>
2020-11-25 12:15:09 +0530

    Added: Welcome Message to master Program

---
+++ EmployeeWageCalculation.java
@@ -0,0 +1,11 @@
+package com.AssignmentDay19_EmployeeWage.EmployeeWage;
+
+public class EmployeeWageCalculation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("!!!!!!!!!! Welcome to Employee Wage Program !!!!!!!!!!");
+
+	}
+
+}
