e4d950af6ad50c19f4090b58c1f4143e2624a07e
abh224272 <abh224272@gmail.com>
2020-11-25 12:43:02 +0530

    Added: Calculation of Daily Wage

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -8,16 +8,22 @@ public class EmployeeWageCalculation {
 		
 		System.out.println("!!!!!!!!!! Welcome to Employee Wage Program !!!!!!!!!!");
 		
+		int WAGE_PER_HOUR=20;
+		int workHour=8;
+		int perDaySalary=0;
+		
 		int isPresent=1;
 		Random random = new Random();
 		int randomValue=random.nextInt(2);
 		
 		if(randomValue==isPresent) {
+			perDaySalary=WAGE_PER_HOUR*workHour;
 			System.out.println("Employee Is Present");
+			System.out.println("Salary= "+perDaySalary);
 		}
 		else {
 			System.out.println("Employee is Absent");
+			System.out.println("Salary= "+perDaySalary);
 		}
 	}
-
 }
