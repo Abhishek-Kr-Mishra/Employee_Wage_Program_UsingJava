7e9c043bc87999c2a18c2ff1ae2107b1f97713fe
abh224272 <abh224272@gmail.com>
2020-11-25 12:28:16 +0530

    Added: Checking Employee is present or Absent

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -1,11 +1,23 @@
 package com.AssignmentDay19_EmployeeWage.EmployeeWage;
 
+import java.util.Random;
+
 public class EmployeeWageCalculation {
 
 	public static void main(String[] args) {
 		
 		System.out.println("!!!!!!!!!! Welcome to Employee Wage Program !!!!!!!!!!");
-
+		
+		int isPresent=1;
+		Random random = new Random();
+		int randomValue=random.nextInt(2);
+		
+		if(randomValue==isPresent) {
+			System.out.println("Employee Is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
